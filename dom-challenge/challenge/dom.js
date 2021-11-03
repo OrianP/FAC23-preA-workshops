bfcde0255edcdec296f75ede528c6af0f3d2acfe
Orian <77785778+OrianP@users.noreply.github.com>
2021-11-03 13:47:01 +0000

    Complete 3b. Amend 3a Add an else statement to remove the 'aria-invalid' attribute if textarea textlength is under 140

--- dom-challenge/challenge/dom.js
+++ dom-challenge/challenge/dom.js
@@ -38,6 +38,8 @@ const generateList = (array) => {
 function validateTweet(textarea) {
   if (textarea.textLength > 140) {
     textarea.setAttribute('aria-invalid', 'true');
+  } else {
+    textarea.removeAttribute('aria-invalid');
   }
 }
 
@@ -46,7 +48,8 @@ function validateTweet(textarea) {
 2. Whenever a user types into it validate it using the previous function
 */
 function validateTweetOnInput() {
-  // your code here
+  const tweet = document.querySelector('textarea#tweet');
+  tweet.addEventListener('input', () => validateTweet(tweet));
 }
 
 /* CHALLENGE 4
