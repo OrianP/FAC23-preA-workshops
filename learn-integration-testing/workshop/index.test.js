6e2683aae3b37d729f1a2fb8292bd46fb5bafe70
Orian <77785778+OrianP@users.noreply.github.com>
2021-11-23 15:18:39 +0000

    Add UI logic test

--- learn-integration-testing/workshop/index.test.js
+++ learn-integration-testing/workshop/index.test.js
@@ -24,3 +24,25 @@ test('Calculate should divide two numbers', () => {
 test('Calculate should handle numbers passed as strings', () => {
     equal(calculate('10', '+', '5'), 15);
 });
+
+// Tests for UI
+// Check that the calculator calculates the result of user input and updates the page
+
+test('Calculator performs division and updates page', () => {
+    // get all inputs and submit button
+    const inputA = document.querySelector('input[name="a"]');
+    const inputB = document.querySelector('input[name="b"]');
+    const select = document.querySelector('select');
+    const submit = document.querySelector('button[type="submit"]')
+    // enter numbers
+    inputA.value = 10;
+    inputB.value = 5;
+    // choose select option
+    select.selectedIndex = 3;
+    // click submit button
+    submit.click();
+    // check if result is equal to expected
+    equal(result.textContent, '2');
+    // clear result display
+    result.textContent = '';
+}) 
