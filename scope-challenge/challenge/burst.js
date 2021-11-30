d119d12ccca909461686f26d58b9107479083951
Orian <77785778+OrianP@users.noreply.github.com>
2021-11-30 15:35:14 +0000

    Add comments

--- scope-challenge/challenge/burst.js
+++ scope-challenge/challenge/burst.js
@@ -1,23 +1,30 @@
-// set getRandomHue to function that generates a number from a range
+// assign getRandomHue to a function that generates a number from a range
+// randomRange returns a callback
 const getRandomHue = randomRange(0, 360);
+// flag for running or stopping animation
 let mousedown = false;
 
 document.addEventListener("mousedown", (event) => {
   mousedown = true;
+  // destrucutre variables from x and y mouseclick coordinates
   const { clientX, clientY } = event;
+  // create burst element at coordinates
   const burst = createBurst(clientX, clientY);
   document.body.appendChild(burst);
 });
 
+// change flag to false when mouseup event fires
 document.addEventListener("mouseup", () => mousedown = false);
 
 function createBurst(x, y) {
   const el = document.createElement("div");
   el.classList.add("burst");
 
+  // generate random number to set as hue
   const hue = getRandomHue();
   el.style.setProperty("--hue", hue);
   
+  // fix coordinates that were switched 
   const top = y - 4;
   const left = x - 4;
   el.style.setProperty("top", top + "px");
@@ -28,11 +35,14 @@ function createBurst(x, y) {
   function grow() {
     size += 1;
     el.style.setProperty("transform", `scale(${size})`);
+    // continue animating while mouse is down
     if (mousedown) {
       requestAnimationFrame(grow);
     }
   }
-
+  // call requestAnimationFrame and return the burst element
   requestAnimationFrame(grow);
   return el;
 }
+
+// Question: can you call a callback with arguments inside            requestAnimationFrame?
